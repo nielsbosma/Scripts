1937564c8a866622585d1795f671a04a3c2dec6c
nielsbosma <niels@bosmainteractive.se>
2026-03-08 18:10:06 +0100

    feat(ivy): add --browse flag to Docs and Samples shortcuts

--- IvyDocs.ps1
+++ IvyDocs.ps1
@@ -1,1 +1,1 @@
-dotnet run --project "D:\Repos\_Ivy\Ivy-Framework\src\Ivy.Docs\Ivy.Docs.csproj" @args
+dotnet run --project "D:\Repos\_Ivy\Ivy-Framework\src\Ivy.Docs\Ivy.Docs.csproj" -- --browse @args
