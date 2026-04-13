f4b8f9df3b951c83971ec2f8ebcd8082082132d0
nielsbosma <niels@bosmainteractive.se>
2026-04-13 08:17:22 +0200

    Update Tendril launch script to set working directory

--- AF2/Tendril.ps1
+++ AF2/Tendril.ps1
@@ -1,1 +1,2 @@
-dotnet run --project "D:\Repos\_Ivy\Ivy-Framework\src\tendril\Ivy.Tendril" -- --find-available-port --browse --enable-dev-tools
+Set-Location "D:\Repos\_Ivy\Ivy-Framework\src\tendril\Ivy.Tendril"
+dotnet run -- --find-available-port --browse --enable-dev-tools
