f5ce9ee7ead5740d0a11b1c0dbb24722c52943ca
nielsbosma <niels@bosmainteractive.se>
2026-04-16 15:39:50 +0200

    Update Tendril launch script path to Ivy-Tendril repo

--- AF2/Tendril.ps1
+++ AF2/Tendril.ps1
@@ -1,6 +1,6 @@
 param([Parameter(ValueFromRemainingArguments)][string[]]$Args)
 
-Set-Location "D:\Repos\_Ivy\Ivy-Framework\src\tendril\Ivy.Tendril"
+Set-Location "D:\Repos\_Ivy\Ivy-Tendril\src\Ivy.Tendril"
 if ($Args.Count -gt 0) {
     dotnet run @Args
 } else {
